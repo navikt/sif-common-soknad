74aa67b7a2832a88cf7537765b89e7dcf723b583
Frode Hansen <frode.hansen@bekk.no>
2020-10-07 20:06:49 +0200

    Copy components from sif-common-core

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import '@formatjs/intl-pluralrules/polyfill';
 import 'nav-frontend-skjema-style';
 import { Normaltekst } from 'nav-frontend-typografi';
-
 require('@formatjs/intl-pluralrules/locale-data/nb');
 require('@formatjs/intl-pluralrules/locale-data/nn');
 
-const App: React.FC = () => {
+const App = () => {
     return <Normaltekst tag="div">Init @navikt/sif-common-soknad</Normaltekst>;
 };
 
---
+++ src/dev/example-soknad/ExampleSoknad.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+
+const ExampleSoknad = ({}: Props) => <div />;
+
+export default ExampleSoknad;
