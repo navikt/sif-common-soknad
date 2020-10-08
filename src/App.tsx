1abc7e5d1e2325bdf5fa5f14a49af138b612024c
Frode Hansen <frode.hansen@bekk.no>
2020-10-08 07:49:55 +0200

    Include global styling + add exports

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import '@formatjs/intl-pluralrules/polyfill';
 import 'nav-frontend-skjema-style';
 import { Normaltekst } from 'nav-frontend-typografi';
 
+import '@navikt/sif-common-core/lib/styles/globalStyles.less';
+
 require('@formatjs/intl-pluralrules/locale-data/nb');
 require('@formatjs/intl-pluralrules/locale-data/nn');
 
